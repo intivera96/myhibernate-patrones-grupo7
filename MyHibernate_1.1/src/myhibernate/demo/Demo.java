70ea475ada4aad362a3fd6554c8ec58dbe98db46
intivera96 <31231659+intivera96@users.noreply.github.com>
2020-08-04 23:05:36 -0300

    Update Demo.java

--- MyHibernate_1.1/src/myhibernate/demo/Demo.java
+++ MyHibernate_1.1/src/myhibernate/demo/Demo.java
@@ -1,5 +1,7 @@
 package myhibernate.demo;
 
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 import myhibernate.MyHibernate;
@@ -9,11 +11,11 @@ public class Demo
 {
    public static void main(String[] args)
    {
-      // primer caso: busqueda por id
+     // primer caso: busqueda por id
+	  DetalleOrden d = MyHibernate.find(DetalleOrden.class,4);
       PromocionVigencia a = MyHibernate.find(PromocionVigencia.class,10);
       Categoria b = MyHibernate.find(Categoria.class,2);
       Cliente c = MyHibernate.find(Cliente.class,5);
-      DetalleOrden d = MyHibernate.find(DetalleOrden.class,4);
       Orden e = MyHibernate.find(Orden.class,1);
       Producto f = MyHibernate.find(Producto.class,14);
       Promocion g = MyHibernate.find(Promocion.class,3);
@@ -21,30 +23,40 @@ public class Demo
       Proveedor i = MyHibernate.find(Proveedor.class,4);
       ProveedorCategoria j = MyHibernate.find(ProveedorCategoria.class,7);
       TipoCliente k = MyHibernate.find(TipoCliente.class,2);
-      Empleado l = MyHibernate.find(Empleado.class,3);
-      System.out.println("Hola Mundo");
-
-/*      // segundo caso: recuperar todas las filas
+      Empleado l = MyHibernate.find(Empleado.class,0);
+      
+     // segundo caso: recuperar todas las filas
       List<Producto> lst = MyHibernate.findAll(Producto.class);
       for(Producto px:lst)
       {
          System.out.println(px.getDescripcion()+", "+px.getProveedor().getEmpresa());         
       }
       
-      // tercer caso: HQL
+      List<DetalleOrden> lst2 = MyHibernate.findAll(DetalleOrden.class);
+      for(DetalleOrden px:lst2)
+      {
+         System.out.println(px.getIdDetalleOrden());  
+      }
+      
+	  List<Proveedor> lst3 = MyHibernate.findAll(Proveedor.class);
+	  for(Proveedor px:lst3)
+	  {
+	     System.out.println(px.getIdProveedor()+", "+px.getEmpresa());  
+	  }
+	     
+     // tercer caso: HQL
       String hql="";
-      hql+="FROM Producto p ";
-      hql+="WHERE p.proveedor.empresa=:emp ";
+      hql+="FROM Producto p JOIN p.proveedor pv ";
+      hql+="WHERE p.id_producto=:id OR (p.pv.empresa = :emp AND p.pv.id_proveedor = :idp)";
       Query q = MyHibernate.createQuery(hql);
+      q.setParameter("id", 7);
       q.setParameter("emp","Sony");
-      List<Producto> lst2 = q.getResultList();
-      for(Producto px:lst2)
+      q.setParameter("idp", 5);
+      List<Producto> lst4 = q.getResultList();
+      for(Producto px:lst4)
       {
          System.out.println(px.getDescripcion()+", "+px.getProveedor().getEmpresa());         
       }
-      
-      
-      */
     
    }
 }
